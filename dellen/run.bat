f3b34de06738c322a5dcdd3834b592a6de0a2ad8
SiebeBosch <siebe@hydroconsult.nl>
2021-07-05 22:57:31 +0200

    'snapping_settings_improved'

--- dellen/run.bat
+++ dellen/run.bat
@@ -1,4 +1,4 @@
 @ echo off
 set OMP_NUM_THREADS=2
-call "c:\Program Files (x86)\Deltares\D-HYDRO Suite 1D2D (Beta) (0.9.9.52575)\plugins\DeltaShell.Dimr\kernels\x64\dimr\scripts\run_dimr.bat"
+call "c:\Program Files\Deltares\D-HYDRO Suite 1D2D (Beta) (0.9.11.53157)\plugins\DeltaShell.Dimr\kernels\x64\dimr\scripts\run_dimr.bat"
 pause
