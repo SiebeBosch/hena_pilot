4e66c9d65b3d61666152be05dccba8f48eb2320b
SiebeBosch <siebe@hydroconsult.nl>
2021-08-30 14:23:11 +0200

    'interval_controller'

---
+++ dellen met timecontrollers/dimr_bat.bat
@@ -0,0 +1,5 @@
+@ echo off 
+    call "c:\Program Files\Deltares\D-HYDRO Suite 1D2D (1.0.0.53506)\plugins\DeltaShell.Dimr\kernelsd\dimr\scriptsun_dimr.bat" dimr_config.xml -d 0 > dimr.log 2>&1 
+    pause
+    :end 
+    
---
+++ dellen met timecontrollers/run.bat
@@ -0,0 +1,4 @@
+@ echo off
+set OMP_NUM_THREADS=2
+call "c:\Program Files\Deltares\D-HYDRO Suite 1D2D (Beta) (0.9.11.53157)\plugins\DeltaShell.Dimr\kernels\x64\dimr\scripts\run_dimr.bat"
+pause
