4e8af6c1eaa74bc0ab064d0f6ecf2cb55a4d656c
SiebeBosch <siebe@hydroconsult.nl>
2021-09-09 22:52:12 +0200

    'debugging_delft3dfmpy'

--- dellen/run.bat
+++ dellen/run.bat
@@ -1,4 +1,4 @@
 @ echo off
 set OMP_NUM_THREADS=2
-call "c:\Program Files\Deltares\D-HYDRO Suite 1D2D (Beta) (0.9.11.53157)\plugins\DeltaShell.Dimr\kernels\x64\dimr\scripts\run_dimr.bat"
+call "c:\Program Files\Deltares\D-HYDRO Suite 1D2D (1.0.0.53506)\plugins\DeltaShell.Dimr\kernels\x64\dimr\scripts\run_dimr.bat"
 pause
